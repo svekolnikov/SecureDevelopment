6c86d381c10423865518f59cd08aef6889eb9be5
Alexey <alexey.svekolnikov@gmail.com>
2022-01-18 17:59:49 +0300

    controller

---
+++ Lesson1/Controllers/NotesEFController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1.Controllers
+{
+    [Route("api/ef")]
+    [ApiController]
+    public class NotesEFController : ControllerBase
+    {
+        [HttpGet("notes")]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        
+        [HttpGet("notes/{id}")]
+        public string GetById(int id)
+        {
+            return "value";
+        }
+        
+        [HttpPost]
+        public void AddAsync([FromBody] string value)
+        {
+        }
+        
+        [HttpPut("notes/{id}")]
+        public void Update(int id, [FromBody] string value)
+        {
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
